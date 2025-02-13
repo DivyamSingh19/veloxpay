a138246721ade40b2ddf6fd111a7392990a4f42c
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-13 20:17:44 +0530

    Updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from './components/Button'
 import './App.css'
-
+import { Route,Routes, useNavigate } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import Profile from './pages/Profile'
+import Payment from './pages/Payment'
+import Home from './pages/Home'
+import { useEffect } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+       
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/payment' element={<Payment/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+         <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+function NotFound(){
+  const navigate = useNavigate();
+  const clickHandler=()=>{
+      navigate("/")
+  }
+  useEffect(()=>{
+    document.body.style.overflow="hidden"
+    return()=>(
+      document.body.style.overflow="auto"
+    )
+  },[])
+  return (
+    <div className='bg-black w-screen h-screen absolute top-0 left-0'>
+
+      <span className='flex items-center justify-center text-white pt-60 font-bold text-4xl' > 404 | Page not found </span>
+      <div className='pt-10'> 
+      <Button label={'Return to Home'} onClick={clickHandler} className='relative bg-white text-black font-medium px-2 py-2 hover:bg-gray-200 '></Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
