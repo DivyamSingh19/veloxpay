0cb6cd46784db3f1a4fbd02f09a8f58ded6bf08f
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-22 18:17:59 +0530

    main

--- server/Server.ts
+++ server/Server.ts
@@ -1,6 +1,7 @@
- 
+import express from "express"
+import { Request , Response } from "express";
 import 'dotenv/config'
-const express = require("express")
+
 
 const app = express();
 const port  = process.env.PORT||4000
@@ -10,10 +11,12 @@ const port  = process.env.PORT||4000
 
 
 app.use(express.json())
+app.get("/",(req:Request,res:Response)=>{
+    res.send("API WORKING")
+})
 
 
 
 
 
-
-app.listen(port,()=>(`Server started on port :${port} - http://localhost:${port}`))
+app.listen(port,()=>(console.log(`Server started on port :${port} - http://localhost:${port}`)))
