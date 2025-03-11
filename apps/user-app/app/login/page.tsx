35c58a15768111e6a5fd857b5aca4029b200579b
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-11 15:37:10 +0530

    updates

---
+++ apps/user-app/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@repo/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button children={undefined} appName={''}>Login</Button>
+    </div>
+  )
+}
+
+export default page
+ 
