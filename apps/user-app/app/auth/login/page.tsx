54b5ce45ff79c894e4161ee57e62ddbccd833358
Divyam Singh <divyamsingh1918@gmail.com>
2025-11-25 13:03:46 +0530

    arch-setup

--- apps/user-app/app/auth/login/page.tsx
+++ apps/user-app/app/auth/login/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from '@repo/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <Button children={undefined} appName={''}>Login</Button>
+      
     </div>
   )
 }
 
 export default page
- 
